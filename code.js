fd5797446458bcd0f0b2adb0086a41b8b3cf2a48
ArtToVideoGames <iamhaile222@gmail.com>
2022-03-09 00:16:20 -0600

    Added Game Master functionality

--- code.js
+++ code.js
@@ -10,7 +10,14 @@ let charSpots = [];
 
 let active_row = 0;
 
+let word_list = ["amber", "pearl", "crane", "helps", "mason", "prank", "plots", "warns", "lucks"];
+let the_word = word_list[Math.floor(Math.random() * word_list.length)];
 let user_word = [];
+
+let green_letters = [];
+let yellow_letters = [];
+let yellow_letters_temp = [];
+let gray_letters = [];
 
 window.onload = function () {
 
@@ -74,7 +81,6 @@ function addLetter(letter) {
                 j = charSpots.length;
             }
         }
-        console.log(user_word.join(''));
     }
     else {
         console.log("Max character limit reached")
@@ -95,5 +101,70 @@ function removeLetter() {
     }
     else {
         console.log("No more letters to remove")
+    }
+}
+
+let k = -1;
+
+function game_checker(gWord, uWord, green, yellow, gray) {
+    if(uWord.length == 5) {
+    setTimeout(function() { 
+        gWord = gWord.toUpperCase();
+
+        let gArray = gWord.split("");
+        k++;
+
+        if(k < uWord.length) {
+            char = uWord[k];
+                if(gArray.includes(char)) {
+                    if(yellow_letters_temp.includes(char)) 
+                    {
+                        gray.push(char);
+                        charSpots[k].setAttribute('id', 'invalid');
+                        game_checker(the_word, user_word, green_letters, yellow_letters, gray_letters);
+                    }
+                    else 
+                    {
+                        if(gArray[k] == char) {
+                            if (green.includes(char)) {
+                                //Do nothing
+                            } 
+                            else {
+                                green.push(char);
+                            }
+                            charSpots[k].setAttribute('id', 'correct');
+                            game_checker(the_word, user_word, green_letters, yellow_letters, gray_letters);
+                        }
+                        else {
+                            yellow.push(char);
+                            charSpots[k].setAttribute('id', 'spot');
+                            yellow_letters_temp = yellow;
+                            game_checker(the_word, user_word, green_letters, yellow_letters, gray_letters);
+                        }   
+                    }
+                }
+                else {
+                    gray.push(char);
+                    charSpots[k].setAttribute('id', 'invalid');
+                    game_checker(the_word, user_word, green_letters, yellow_letters, gray_letters);
+                }
+            } 
+            else {
+                if(green_letters.length < 5) {
+                    active_row++;
+                    rows[active_row].classList.add('active');
+                    charSpots = rows[active_row].children;
+                    user_word = [];
+                    yellow_letters_temp = [];
+                    k = -1;
+                } 
+                else if(green_letters.length >= 5) {
+                console.log("Winner!");
+                }
+            }  
+        }, 250);
+    }
+    else {
+        console.log("5 Letters Needed");
     }
 }
